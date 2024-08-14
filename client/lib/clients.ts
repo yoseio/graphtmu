7b29891c9e2b649de1ce57545ee2b098e678452e
yoseio <98276492+yoseio@users.noreply.github.com>
2024-08-14 17:35:54 +0000

    client: support Workload Identity

--- client/lib/clients.ts
+++ client/lib/clients.ts
@@ -1,14 +1,72 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore"
+import { ExternalAccountClient, GoogleAuth } from "google-auth-library";
 import OpenAI from "openai";
+import { getVercelOidcToken } from '@vercel/functions/oidc';
+
+import { NullToUndefined } from "@/lib/utils";
 
 const FIREBASE_SA_KEY = process.env["FIREBASE_SA_KEY"] || "";
+
+const GCP_PROJECT_ID = process.env["GCP_PROJECT_ID"] || "";
+const GCP_PROJECT_NUMBER = process.env["GCP_PROJECT_NUMBER"] || "";
+const GCP_SERVICE_ACCOUNT_EMAIL = process.env["GCP_SERVICE_ACCOUNT_EMAIL"] || "";
+const GCP_WORKLOAD_IDENTITY_POOL_ID = process.env["GCP_WORKLOAD_IDENTITY_POOL_ID"] || "";
+const GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID = process.env["GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID"] || "";
+
 const OPENAI_API_KEY = process.env["OPENAI_API_KEY"] || "";
 
-if (!getApps()?.length) {
-  initializeApp({
-    credential: cert(JSON.parse(FIREBASE_SA_KEY))
+// https://vercel.com/docs/security/secure-backend-access/oidc/gcp
+function vercelWorkloadIdentity(
+  projectId: string,
+  projectNumber: string,
+  serviceAccountEmail: string,
+  workloadIdentityPoolId: string,
+  workloadIdentityPoolProviderId: string,
+): GoogleAuth {
+  const authClient = NullToUndefined(ExternalAccountClient.fromJSON({
+    type: 'external_account',
+    audience: `//iam.googleapis.com/projects/${projectNumber}/locations/global/workloadIdentityPools/${workloadIdentityPoolId}/providers/${workloadIdentityPoolProviderId}`,
+    subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+    token_url: 'https://sts.googleapis.com/v1/token',
+    service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${serviceAccountEmail}:generateAccessToken`,
+    subject_token_supplier: {
+      getSubjectToken: getVercelOidcToken,
+    },
+  }));
+  return new GoogleAuth({
+    authClient,
+    projectId,
   });
+}
+
+if (!getApps()?.length) {
+  if (FIREBASE_SA_KEY === "") {
+    // Workload Identity
+    const auth = vercelWorkloadIdentity(
+      GCP_PROJECT_ID,
+      GCP_PROJECT_NUMBER,
+      GCP_SERVICE_ACCOUNT_EMAIL,
+      GCP_WORKLOAD_IDENTITY_POOL_ID,
+      GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID,
+    );
+    initializeApp({
+      credential: {
+        getAccessToken: async () => {
+          const adc = await auth.getApplicationDefault();
+          return {
+            access_token: adc.credential.credentials.access_token!,
+            expires_in: adc.credential.credentials.expiry_date!,
+          }
+        },
+      }
+    });
+  } else {
+    // Service Account Key
+    initializeApp({
+      credential: cert(JSON.parse(FIREBASE_SA_KEY))
+    });
+  }
 }
 export const FirestoreClient = getFirestore();
 
--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function NullToUndefined<T>(value: T | null): T | undefined {
+  return value === null ? undefined : value;
+}
